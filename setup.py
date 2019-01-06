f844808e63b8fc290391cf5281494f24183d655b
Matt Bachmann <bachmann.matt@gmail.com>
2019-01-05 20:36:52 -0500

    version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name='marshmallow-polyfield',
-    version=5.2,
+    version=5.3,
     description='An unofficial extension to Marshmallow to allow for polymorphic fields',
     long_description=read('README.rst'),
     author='Matt Bachmann',
